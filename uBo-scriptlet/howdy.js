97ec1709c445b8dbc8711b1a12d64cb99f59654f
zbluebugz <51784903+zbluebugz@users.noreply.github.com>
2022-10-09 00:01:13 +0100

    Update howdy.js

--- uBo-scriptlet/howdy.js
+++ uBo-scriptlet/howdy.js
@@ -1,2 +1,9 @@
 /// howdy.js
-console.info("howdy howdy!!! :-) I'm running via scriptlet injection.");
+if (document.readyState != "complete"){
+        window.addEventListener('load', sayHowdy, false); 
+}else{
+        sayHowdy()
+}
+function sayHowdy() {
+  console.info("howdy howdy!!! :-) I'm running via scriptlet injection.");
+}
